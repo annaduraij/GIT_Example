c38699abaf4af276953801322128d4408eb5a842
annaduraij <122567409+annaduraij@users.noreply.github.com>
2023-03-02 11:28:57 -0500

    Initial Push!

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Author: Jay Annadurai | Jenna Horrall
+ * Date: 02 Mar 2023
+ * Project: GIT Example
+ * File: index.php
+ * Description:
+ */ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <style></style>
+</head>
+
+<body>
+
+<?php
+
+//This is Jay Annadurai!
+echo 'Hi! This is Jay!';
+
+
+?>
+
+</body>
+
+</html>
